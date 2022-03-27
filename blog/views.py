bccf3f9480da0a7a5ecbcbe8448fa9d5f4fd0e91
Jakha921 <jakha921@gmail.com>
2022-03-28 02:24:35 +0500

    MTV

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the blog index.")
+
+def category(request):
+    return HttpResponse("<h1>You're looking at category.</h1>")
