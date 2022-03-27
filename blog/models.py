b6a0b66447f2eac1735094716fcd51aec6bc8fb8
Jakha921 <jakha921@gmail.com>
2022-03-28 03:51:06 +0500

    create model & migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Men (models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    time_create = models.DateField(auto_now_add=True)
+    time_update = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from blog import views
+from config import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
 
 # handlers
 handler404 = views.page_not_found
