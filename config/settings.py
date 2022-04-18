a642ad28d360a315c3bdc24f225d7c1cf8e05061
Jakha921 <jakha921@gmail.com>
2022-04-19 01:53:57 +0500

    connect with static files and use filters

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'      # URL to access static files (server-side)
+STATIC_ROOT = BASE_DIR / 'static/'   # Absolute path to static files (localhost)
+STATICFILES_DIRS = [ BASE_DIR / 'static_files' ]   # Absolute path to static files (if have any other static files in other folders)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
