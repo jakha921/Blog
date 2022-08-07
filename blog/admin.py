f2a989a04072ed3a6d3c8fd6bc972ce1aa3f2a9d
Jakha921 <jakha921@gmail.com>
2022-08-07 06:06:23 +0500

    customize in detail admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Men, Category
 
 # Register your models here.
@@ -6,12 +8,28 @@ from .models import Men, Category
 
 @admin.register(Men)
 class MenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'photo', 'time_create', 'is_published',)
+    list_display = ('id', 'title', 'get_photo', 'time_create', 'is_published',)
     list_display_links = ('id', 'title',)
     search_fields = ('title', 'content',)
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create',)
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('title',)}      # генерация slug url ASCII
+    fields = ('title', 'slug', 'content', 'photo', 'get_photo', 'time_create', 'time_update', 'is_published', 'category', )
+    readonly_fields = ('time_create', 'time_update', 'get_photo')
+    # radio_fields = {"category": admin.VERTICAL}   # radio btn
+    save_on_top = True                            # btn save on top
+    autocomplete_fields = ['category']              # search field FK 
+    save_as = True
+    search_help_text = 'Search title & content'     # show help text under search field
+    view_on_site = True                            # link for this content if have  get_absolute_url()
+    actions_selection_counter = True                # counter selected elements
+
+    
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="60"')
+            
+    get_photo.short_description = 'img'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,3 +37,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name',)
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.site_header = 'Admin panel for a blog about Men'
+admin.site.site_title = 'Blog admin panel'
